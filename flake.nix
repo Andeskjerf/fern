7bda3a9f1d055b6c3d2a6e6eafcb0c3a09c43930
Andeskjerf <6963524+Andeskjerf@users.noreply.github.com>
2026-09-05 14:46:09 +0200

    flake add packages

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        toolchain = fenix.packages.${system}.stable.withComponents [
+        devToolchain = fenix.packages.${system}.stable.withComponents [
           "cargo"
           "clippy"
           "rust-src"
@@ -27,10 +27,22 @@
           "rustfmt"
           "rust-analyzer"
         ];
+        buildToolchain = fenix.packages.${system}.stable.minimalToolchain;
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = buildToolchain;
+          rustc = buildToolchain;
+        };
       in
       {
         devShells.default = pkgs.mkShell {
-          packages = [ toolchain ];
+          packages = [ devToolchain ];
+        };
+
+        packages.default = rustPlatform.buildRustPackage {
+          pname = "fern";
+          version = "0.1.0";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
         };
       }
     );
