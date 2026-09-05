685b769e8d7c887eb9379b899483e5a5f3fd2d45
Andeskjerf <6963524+Andeskjerf@users.noreply.github.com>
2026-09-05 14:30:37 +0200

    setup dev shell nix

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  inputs = {
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      fenix,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        toolchain = fenix.packages.${system}.stable.withComponents [
+          "cargo"
+          "clippy"
+          "rust-src"
+          "rustc"
+          "rustfmt"
+          "rust-analyzer"
+        ];
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [ toolchain ];
+        };
+      }
+    );
+}
