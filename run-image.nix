bf6c1971b8d1525ec1f69d68a632be0328b6c9bc
Andeskjerf <6963524+Andeskjerf@users.noreply.github.com>
2026-09-06 13:01:25 +0200

    create overlay and qcow2 base image

--- run-image.nix
+++ run-image.nix
@@ -9,13 +9,22 @@ writeShellScriptBin "repart-image-qemu" ''
   set -euo pipefail
 
   mkdir -p images
+  BASE_IMAGE="base.qcow2"
   DISK_IMAGE="images/worker-overlay.qcow2"
 
-  if [[ ! -f "$DISK_IMAGE" ]]; then
-    ${qemu}/bin/qemu-img create -f qcow2 \
-      -b ${image}/image.raw -F raw $DISK_IMAGE
-      ${qemu}/bin/qemu-img resize -f qcow2 "$DISK_IMAGE" "+10G"
+  if [[ ! -f "images/$BASE_IMAGE" ]]; then
+    ${qemu}/bin/qemu-img convert -f raw \
+      -O qcow2 -c ${image}/image.raw "images/$BASE_IMAGE"
+      ${qemu}/bin/qemu-img resize -f qcow2 "images/$BASE_IMAGE" "+10G"
   fi
+
+  if [[ -f "$DISK_IMAGE" ]]; then
+    rm "$DISK_IMAGE"
+  fi
+
+  ${qemu}/bin/qemu-img create -f qcow2 \
+    -b "$BASE_IMAGE" -F qcow2 "$DISK_IMAGE"
+    ${qemu}/bin/qemu-img resize -f qcow2 "$DISK_IMAGE" "+10G"
 
   ${qemu}/bin/qemu-system-x86_64 \
     -smp 4 \
